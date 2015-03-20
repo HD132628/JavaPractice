0e30ead3c6160e9ca360b4dc794bd303482d189a
wjdxo_000 <wjdxo_000@Taehan-computer>
2015-03-20 10:41:01 +0900

    수정본

--- JavaPractice/src/org/dimigo/basic/Operator.java
+++ JavaPractice/src/org/dimigo/basic/Operator.java
@@ -12,7 +12,10 @@ public class Operator {
 		System.out.println("사다리꼴 넓이 : " + s1);
 		System.out.println("평행사변형 넓이 : " + s2);
 		System.out.println("");
-		System.out.println("사다리꼴이 평행사변형 보다 " + (s1-s2) + " 더 큽니다.");
+		if(s1 < s2)
+			System.out.println("평행사변형이 사다리꼴 보다 " + (s2-s1) + " 더 큽니다.");
+		else
+			System.out.println("사다리꼴이 평행사변형 보다 " + (s1-s2) + " 더 큽니다.");
 		
 	}
 
