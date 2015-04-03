fa15c0b763dae0d26e06b18bc6b1985224f694a0
wjdxo_000 <wjdxo_000@Taehan-computer>
2015-04-03 10:51:34 +0900

    헿

---
+++ JavaPractice/src/org/dimigo/basic/Loop.java
@@ -0,0 +1,23 @@
+package org.dimigo.basic;
+
+public class Loop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i,j;
+		for(i=1;i<=10;i++)
+		{
+			for(j=1;j<=10;j++)
+			{
+				if(i==j)
+				{
+					System.out.printf("*");
+					continue;
+				}
+				System.out.printf("%d",j);
+			}
+			System.out.println("");
+		}
+	}
+
+}
