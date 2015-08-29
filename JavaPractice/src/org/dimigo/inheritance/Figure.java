68ae1646d670bd8a8fc1ebe834fc04cdd2e32918
wjdxo_000 <wjdxo_000@Taehan-computer>
2015-08-29 21:37:03 +0900

    asdasd

---
+++ JavaPractice/src/org/dimigo/inheritance/Figure.java
@@ -0,0 +1,23 @@
+package org.dimigo.inheritance;
+
+public class Figure { 
+ 	private int centerX; 
+ 	private int centerY; 
+ 	public Figure(int centerX,int centerY){ 
+ 		this.centerX=centerX; 
+ 		this.centerY=centerY; 
+ 	} 
+ 	protected double calcArea(){ 
+ 		return 0; 
+ 		 
+ 	} 
+ 	protected void printCenter(){ 
+ 		System.out.println("Áß½ÉÁÂÇ¥:("+centerX+","+centerY+")"); 
+ 	} 
+ 	protected void moveFigure(int x,int y){ 
+ 		centerX+=x; 
+ 		centerY+=y; 
+ 	} 
+ 
+ 
+} 
